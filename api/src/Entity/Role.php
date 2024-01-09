91d2867741da60bfb047a699c464ead834479a86
Kbakazami <struys.wyvin@gmail.com>
2024-01-09 12:27:49 +0100

    feature/CSM-09 - Création de l'entité Rôle
    - Add: Entity Role

---
+++ api/src/Entity/Role.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoleRepository::class)]
+#[ApiResource]
+class Role
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
